b98d406d8a3731ba61cc159e59f551836e3094d1
Your Name <a01425316@tec.mx>
2025-04-09 10:54:08 -0600

    new create, getActive and inactive projetcs endpoints

--- backend/src/routes/researcher.js
+++ backend/src/routes/researcher.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { getActiveProjects } = require('../controllers/researcherController')
+const { getActiveProjects, getInactiveProjects, createProject } = require('../controllers/researcherController')
 const middleware = require('../middleware/jwt.middleware')
 
-router.get('/:userId/projects', getActiveProjects)
+router.get('/:userId/projects/active', getActiveProjects)
+router.get('/:userId/projects/inactive', getInactiveProjects)
+router.post('/projects/create', createProject)
 
 module.exports = router
