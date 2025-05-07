cb08585b9648c0f14022af8d2714acfba0aabca2
Your Name <a01425316@tec.mx>
2025-05-06 20:15:50 -0600

    un pequeño problema

--- backend/src/middleware/multer.middleware.js
+++ backend/src/middleware/multer.middleware.js
@@ -1,5 +1,5 @@
 const multer = require('multer');
-const { uploadDocuments } = require('../controllers/researcherController');
+const { uploadDocuments } = require('../controllers/researcher.controller');
 const storage = multer.memoryStorage() //esto es para almacenar temporalemnte los archivos en la RAM como buffers, y tal vez te preguntes que es un buffer?, no se
 const upload = multer({ storage }).array('documents'); //nombre del input para el frontend (Gordinho13)
 
