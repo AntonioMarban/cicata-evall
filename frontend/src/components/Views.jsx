3ffc36e1e0ed45de72fc10db9fead0e06309a9fc
Luis-Navarro13 <a01663776@tec.mx>
2025-05-12 22:09:46 -0600

    header complete form

--- frontend/src/components/Views.jsx
+++ frontend/src/components/Views.jsx
@@ -8,7 +8,7 @@ import ManageIndividualUserForm from "../pages/ManageIndividualUserForm";
 import EditRubric from "../pages/EditRubric";
 import EvaluateProject from "../pages/EvaluateProject";
 import CommitteeDictumForm from "./CommitteeDictumForm";
-import ViewCompleteForms from "./ViewCompleteForms";
+import ViewCompleteForm from "./ViewCompleteForm";
 import Project from "../pages/Project";
 import CommentsCommittee from "../pages/CommentsCommittee";
 const Views = () => {
@@ -22,7 +22,7 @@ const Views = () => {
     <Route path = "/FormularioDeUsuario" element={<ManageIndividualUserForm/>}></Route>
     <Route path = "/EditarRubrica" element={<EditRubric/>}></Route>
     <Route path = "/EvaluarProyecto" element={<EvaluateProject/>}></Route>
-    <Route path = "/VerFormulario/:id" element={<ViewCompleteForms/>}></Route>
+    <Route path = "/VerFormulario/:id" element={<ViewCompleteForm/>}></Route>
     <Route path = "/Proyecto" element={<Project/>}></Route>
     <Route path = "/Forms" element={<CommitteeDictumForm/>}></Route>
     <Route path = "/comentarios" element={<CommentsCommittee/>}></Route>
