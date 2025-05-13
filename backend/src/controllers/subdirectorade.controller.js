25017de06d7cfcdac43e6b731df4edf5a8216703
Carolina Figueroa <andreacarolina.f28@gmail.com>
2025-05-13 12:12:29 -0600

    response fixed

--- backend/src/controllers/subdirectorade.controller.js
+++ backend/src/controllers/subdirectorade.controller.js
@@ -279,7 +279,7 @@ const getResultThirdStage = async (req, res) => {
     if (results.length === 0) {
       return res.status(404).json({ error: "Resource does not exist" });
     }
-    res.status(200).json(results[0]);
+    res.status(200).json(results[0][0]);
   })
 }
 
