d72f0d94f56c00a963281af17f014a803180b42b
Your Name <a01425316@tec.mx>
2025-05-15 12:53:30 -0600

    Storage changes in documents endpoint

--- backend/src/middleware/multer.middleware.js
+++ backend/src/middleware/multer.middleware.js
@@ -1,6 +1,6 @@
 const multer = require('multer');
 const { uploadDocuments } = require('../controllers/researcher.controller');
 const storage = multer.memoryStorage() //esto es para almacenar temporalemnte los archivos en la RAM como buffers, y tal vez te preguntes que es un buffer?, no se
-const upload = multer({ storage }).array('documents'); //nombre del input para el frontend (Gordinho13)
+const upload = multer({ storage, limits: { fileSize: 50 * 1024 * 1024 } }).array('documents'); //nombre del input para el frontend (Gordinho13), y aumente el limite del tamaño
 
 module.exports = upload
