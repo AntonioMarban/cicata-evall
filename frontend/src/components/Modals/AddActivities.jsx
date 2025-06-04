51f52b06cd7faf8924453a9e73ee4ee25e7aba19
Luis-Navarro13 <a01663776@tec.mx>
2025-06-04 16:45:07 -0600

    formato dd/mm/aaaa

--- frontend/src/components/Modals/AddActivities.jsx
+++ frontend/src/components/Modals/AddActivities.jsx
@@ -74,7 +74,7 @@ const  AddActivities = ({setActivities, activitesToEdit = null, onEditComplete =
         const day = String(date.getDate()).padStart(2, '0');
         const month = String(date.getMonth() + 1).padStart(2, '0');
         const year = date.getFullYear();
-        return `${day}-${month}-${year}`;
+        return `${day}/${month}/${year}`;
     }
     }
     const handleSubmit = (e) => {
--- frontend/src/components/ViewForm/Budget.jsx
+++ frontend/src/components/ViewForm/Budget.jsx
@@ -27,7 +27,7 @@ const ConflictInterest = ({Budget}) => {
         const day = String(date.getDate()).padStart(2, '0');
         const month = String(date.getMonth() + 1).padStart(2, '0');
         const year = date.getFullYear();
-        return `${day}-${month}-${year}`;
+        return `${day}/${month}/${year}`;
     }
     
     return value || '-';
