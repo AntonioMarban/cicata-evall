55310f4a1ed26b6372aa7579c15dc53b1fc97077
Iker <iker.landelao@gmail.com>
2025-06-05 10:08:06 -0600

    Edición a los estilos del botón de cursiva

--- frontend/src/components/Editor.jsx
+++ frontend/src/components/Editor.jsx
@@ -36,27 +36,29 @@ const EditorS = ({ content, onUpdate,placeholderI="Escribe algo" }) => {
     }, [content, editor]);
   return (
     <>
-    <div className="flex gap-2 !mt-5 !mb-5">
-    <button
-    onClick={() => editor.chain().focus().toggleItalic().run()}
-    className={`
-        p-2 rounded-md transition-colors
-        ${editor?.isActive('italic') 
-        ? 'cursor-pointer !p-5 bg-[#5CB7E6] text-white' 
-        : 'cursor-pointer !p-5 text-gray-600 hover:bg-gray-100'
-        }
-    `}
-    aria-pressed={editor?.isActive('italic')}
-    title="Cursiva (Ctrl+I)"
-    >
-    <i className={`${editor?.isActive('italic') ? 'font-bold' : ''}`}>
-        Cursiva
-    </i>
-    </button>
-    </div>
-    <div className="w-full h-full p-2.5 rounded-lg border-2 border-[#E1E1E1] transition-colors duration-300 text-lg min-w-[250px]">
-      <EditorContent editor={editor} />
-    </div>
+      <div className="flex gap-2 !mt-5 !mb-3">
+        <button
+        onClick={() => editor.chain().focus().toggleItalic().run()}
+        className={`
+            p-2! rounded-md transition-colors duration-300 border-2 cursor-pointer  text-sm
+            ${editor?.isActive('italic') 
+            ? 'bg-gray-100 hover:bg-gray-300 border-[#888888] font-bold' 
+            : 'text-gray-800 hover:bg-gray-300 border-[#E1E1E1] font-regular'
+            }
+        `}
+        aria-pressed={editor?.isActive('italic')}
+        title="Cursiva (Ctrl+I)"
+        >
+          <i className={`${editor?.isActive('italic') ? 'font-bold' : ''}`}>
+              Cursiva
+          </i>
+        </button>
+      </div>
+      <div className="w-full h-full rounded-lg border-2 border-[#E1E1E1] transition-colors duration-300 text-lg min-w-[250px]">
+        <EditorContent 
+          editor={editor} 
+        />
+      </div>
     </>
   )
 }
