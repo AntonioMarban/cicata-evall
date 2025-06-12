3d7986d202faf9eed917f28007ca4776ade311a8
Iker <iker.landelao@gmail.com>
2025-06-12 00:27:48 -0600

    Pequeña modificación a dictámenes finales

--- frontend/src/components/ProjectDictum/DictumAddress.jsx
+++ frontend/src/components/ProjectDictum/DictumAddress.jsx
@@ -1,5 +1,5 @@
 import formatvalue from "../../hooks/formatValue"
-export default function DictumAddress({ projectOwner, projectOwnerAcademicDegree, authorizationDate }) {
+export default function DictumAddress({ projectOwner, authorizationDate }) {
 
     const formatDate = (dateString) => {
         const date = new Date(dateString);
@@ -13,7 +13,7 @@ export default function DictumAddress({ projectOwner, projectOwnerAcademicDegree
         <div id="dictum-address-text" className="flex flex-col justify-between mt-3!">
             <div className="flex justify-between gap-2 items-start mt-3!">
                 <div className="flex flex-col items-start font-bold w-[45%]">
-                    <p>{projectOwner}, {projectOwnerAcademicDegree}</p>
+                    <p>{projectOwner}</p>
                     <p>Investigador(a) Titular</p>
                     <p>CICATA Unidad Morelos</p>
                     <p>PRESENTE</p>
